e4251f87b238a8646a55bd689f6598bb8b1711b8
Alexis Ordenes <giraalexis@outlook.com>
2020-12-03 20:10:41 -0300

    Agregar manafest de la aplicacion
    agregar iconos
    agregar manifest

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 //Registro del service Worker
 if(navigator.serviceWorker){ //si esta disponible en este navegador
-    if(window.location.href.includes("localhost")){
+    if(window.location.href.includes("localhost") || window.location.href.includes("127.0.0.1")){
         navigator.serviceWorker.register("/sw.js");
     }else
         //esta en un servidor web
